8993abccffacafe52ede853ea488b18fece322b6
Simeon Griggs <simeon@hey.com>
2023-04-22 08:23:15 +0100

    small changes

--- schema/duration/timeValueType.ts
+++ schema/duration/timeValueType.ts
@@ -1,16 +1,5 @@
 import {defineType} from 'sanity'
 
-// A function that generates an array of times from 00:00 to 23:30
-export const ALLOWED_TIMES = () => {
-  const times = []
-  for (let h = 0; h < 24; h++) {
-    for (let m = 0; m < 60; m += 30) {
-      times.push(`${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}`)
-    }
-  }
-  return times
-}
-
 export const timeValueType = defineType({
   name: 'timeValue',
   title: 'Time',
@@ -19,3 +8,14 @@ export const timeValueType = defineType({
     list: ALLOWED_TIMES(),
   },
 })
+
+// A function that generates an array of times from 00:00 to 23:30
+export function ALLOWED_TIMES() {
+  const times = []
+  for (let h = 0; h < 24; h++) {
+    for (let m = 0; m < 60; m += 30) {
+      times.push(`${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}`)
+    }
+  }
+  return times
+}
--- schema/rating/RatingInput.tsx
+++ schema/rating/RatingInput.tsx
@@ -1,32 +1,52 @@
 import {Grid, Button} from '@sanity/ui'
 import {NumberInputProps, set} from 'sanity'
-import {useCallback} from 'react'
-
-const MAX_RATING = 10
+import {useMemo, useCallback} from 'react'
 
 export function RatingInput(props: NumberInputProps) {
-  const {onChange, value} = props
+  const {onChange, schemaType, value} = props
+  const {validation = []} = schemaType
+
+  const range = useMemo(() => generateRange(validation as any[]), [validation])
 
   const handleScore = useCallback(
     (event: React.MouseEvent<HTMLButtonElement>) => {
-      const nextValue = Number(event.currentTarget.value)
-      onChange(set(nextValue))
+      const value = Number(event.currentTarget.value)
+      onChange(set(value))
     },
     [onChange]
   )
 
   return (
-    <Grid columns={MAX_RATING} gap={1}>
-      {Array.from({length: MAX_RATING}).map((_, index) => (
+    <Grid columns={range.length} gap={1}>
+      {range.map((index) => (
         <Button
           key={index}
-          mode={value === index + 1 ? 'default' : 'ghost'}
-          tone={value === index + 1 ? 'primary' : 'default'}
-          text={index + 1}
-          value={index + 1}
+          mode={value === index ? 'default' : 'ghost'}
+          tone={value === index ? 'primary' : 'default'}
+          text={index.toString()}
+          value={index}
           onClick={handleScore}
         />
       ))}
     </Grid>
   )
+}
+/**
+ * Function that finds the `min` and `max` rules from validations,
+ * and generates the range of numbers between them
+ **/
+function generateRange(validation: any[]) {
+  const [min, max] = validation
+    .reduce((acc, {_rules}) => {
+      return [...acc, ..._rules]
+    }, [])
+    .filter((rule: any) => ['max', 'min'].includes(rule.flag))
+    .map((rule: any) => rule.constraint)
+
+  let range = []
+  for (let i = min; i <= max; i++) {
+    range.push(i)
+  }
+
+  return range
 }
